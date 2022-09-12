9256e894212fca8fc312fb54ce7a2cd9833c6fae
onealog <oneadev@gmail.com>
2022-09-12 20:03:17 +0900

    [ADD] prgress 기능 적용

---
+++ src/_project/2-progress-steps/script.js
@@ -0,0 +1,47 @@
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+const circles = document.querySelectorAll('.circle');
+const progress = document.querySelector('#progress');
+
+let count = 0;
+let width = 0;
+
+console.log(circles)
+
+nextButton.addEventListener('click', () => {
+  count++;
+  width += 30;
+
+  if (count > 0) {
+    prevButton.disabled = false;
+  }
+
+  progress.style.width = `${width}%`;
+  circles[count].classList.add('active');
+
+  if (count === 3) {
+    nextButton.disabled = true;
+    return;
+  }
+});
+
+prevButton.addEventListener('click', () => {
+  if (count === 0) {
+    return;
+  }
+
+  if (count === 1) {
+    prevButton.disabled = true;
+    nextButton.disabled = false;
+  }
+
+  if (count > 1) {
+    nextButton.disabled = false;
+  }
+
+  count--;
+  width -= 30;
+
+  progress.style.width = `${width}%`;
+  circles[count + 1].classList.remove('active');
+});
