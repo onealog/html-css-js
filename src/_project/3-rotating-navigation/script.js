4e9d2cecb9d64d43ae688f81f810c6c64aad51c2
onealog <oneadev@gmail.com>
2022-09-18 00:01:32 +0900

    [REFACTOR] circle 아이콘 변화하는 거 js말고 css로 조작하도록 리팩터링

--- src/_project/3-rotating-navigation/script.js
+++ src/_project/3-rotating-navigation/script.js
@@ -1,15 +1,11 @@
 const container = document.querySelector('.container');
-const circle = document.querySelector('.circle');
-
 const openButton = document.getElementById('open');
 const closeButton = document.getElementById('close');
 
 openButton.addEventListener('click', () => {
-  circle.style.transform = 'rotate(-90deg)';
   container.classList.add('show-nav');
 });
 
 closeButton.addEventListener('click', () => {
-  circle.style.transform = 'rotate(0)';
   container.classList.remove('show-nav');
 });
