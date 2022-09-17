317ce1daa7a67f4fb183fe646394dd9bedd4de5b
onealog <oneadev@gmail.com>
2022-09-17 23:56:30 +0900

    [ADD] close 아이콘 눌렀을 때 메뉴 닫히도록 작업

--- src/_project/3-rotating-navigation/script.js
+++ src/_project/3-rotating-navigation/script.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('.container');
-const openButton = document.getElementById('open');
 const circle = document.querySelector('.circle');
+
+const openButton = document.getElementById('open');
+const closeButton = document.getElementById('close');
 
 openButton.addEventListener('click', () => {
   circle.style.transform = 'rotate(-90deg)';
   container.classList.add('show-nav');
+});
+
+closeButton.addEventListener('click', () => {
+  circle.style.transform = 'rotate(0)';
+  container.classList.remove('show-nav');
 });
